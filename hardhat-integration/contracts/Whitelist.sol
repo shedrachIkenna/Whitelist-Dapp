008ad8e6e0aa658ded813a5a2b7dab8eb4483368
shedrachIkenna <shedrach686@gmail.com>
2023-02-21 15:25:29 +0100

    Created Whitelist state variables

--- hardhat-integration/contracts/Whitelist.sol
+++ hardhat-integration/contracts/Whitelist.sol
@@ -6,4 +6,13 @@ contract Whitelist {
     // max number of whitelisted addresses allowed
     // uint8 can only hold unsigned interger values from 0 - 255 using eight 0s and 1s
     uint8 maxWhitelistedAddresses;
+
+    // mapping maps whitelistedAddresses to boolean datatype
+    // if an address is whitelisted, it is set to true, otherwise it is set to false by default
+    mapping(address => bool) whitelistedAddresses;
+
+    // numAddressesWhitelisted keeps track of the number of addresses that have been whitelisted
+    uint8 public numAddressesWhitelisted;
+
+    
 }
