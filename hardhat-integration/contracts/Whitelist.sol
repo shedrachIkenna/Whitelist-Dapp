974a071cd55282da32f46d0765e7b98e0c31d1aa
shedrachIkenna <shedrach686@gmail.com>
2023-02-21 15:16:30 +0100

    Created whitelist contract

---
+++ hardhat-integration/contracts/Whitelist.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    // max number of whitelisted addresses allowed
+    // uint8 can only hold unsigned interger values from 0 - 255 using eight 0s and 1s
+    uint8 maxWhitelistedAddresses;
+}
