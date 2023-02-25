efc6a5394b9827943b7447ba5ff9a85e3859fcde
shedrachIkenna <shedrach686@gmail.com>
2023-02-25 16:21:38 +0100

    Added getProviderOrSigner function logic

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,9 +1,33 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import web3Modal from "web3modal";
+import { useState, useEffect } from "react";
+import { providers, Contract } from "ethers";
  
 
 export default function Home() {
+
+  const [walletConnect, setWalletConnect] = useState(false);
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+    if(chainId !== 5){
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
+
+    if(needSigner){
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  }
+
+
+  
   return (
     <div>
       <Head>
